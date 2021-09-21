2b2d884c7c4255fd5ff3039b4706d8b1c975eba9
Andre Kolber <andrekolber1@gmail.com>
2021-09-21 14:43:27 -0400

    revised jasmine tests

--- tvmaze.test.js
+++ tvmaze.test.js
@@ -1,3 +1,9 @@
-it('should return array of objects containing API movie data', () => {
-	expect(getEpisodes(1)).toEqual();
+it('should return array of objects containing API movie data', async function() {
+	const res = await searchShows();
+	expect(res).toBeInstanceOf(Array);
+});
+
+it('should return array of objects containing API episode data', async function() {
+	const res = await getEpisodes(1);
+	expect(res).toBeInstanceOf(Array);
 });
